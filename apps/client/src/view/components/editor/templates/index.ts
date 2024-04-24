a32b1f742749f94573f46e71a388003ad63bee2d
Vitor Sousa <v.sousa.cf@gmail.com>
2024-04-24 20:36:34 -0300

    feat: :zap: update template with tests

--- apps/client/src/view/components/editor/templates/index.ts
+++ apps/client/src/view/components/editor/templates/index.ts
@@ -1,15 +1,28 @@
 export const templates = [
 	{
-		name: 'First',
+		name: "First",
 		content: [
-			`import 'isomorphic-fetch';`,
+			// `import 'isomorphic-fetch';`,
 			`import chalk from 'chalk';`,
-			'',
-			`console.log(chalk.green("Hello World"));`,
-			`function logTesting() {
-  console.log('Testing');
-      }`,
-			`logTesting();`
-		].join('\n')
-	}
+			"",
+			`export function double(number) {
+		return number * 2;
+}`,
+			"const result = double(3);",
+			"console.log(chalk.green(result));",
+		].join("\n"),
+		test: [
+			`import { spy } from '@vitest/spy';`,
+			`import { double } from './index';`,
+			"",
+			`
+			describe('double', () => {
+				it('should be double' , () =>{
+					const result = double(3);
+					expect(result).toBe(6);
+				})
+			})
+			;`,
+		].join("\n"),
+	},
 ];
